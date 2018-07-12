5e004ec226631a49397a802e3835e1fd94d5f0a4
Ubaidillah Hakim <ubaidillahhf@gmail.com>
2018-07-12 18:42:41 +0700

    headerlogin

--- src/views/login.php
+++ src/views/login.php
@@ -6,15 +6,15 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>IPL Software</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="<? echo base_url('assets/plugins/font-awesome/css/font-awesome.min.css') ?>">
+        <link rel="stylesheet" href="../assets/plugins/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-        <link rel="stylesheet" href="<? echo base_url('assets/dist/css/adminlte.min.css') ?>">
-        <link rel="stylesheet" href="<? echo base_url('assets/plugins/iCheck/flat/blue.css') ?>">
-        <link rel="stylesheet" href="<? echo base_url('assets/plugins/morris/morris.css') ?>">
-        <link rel="stylesheet" href="<? echo base_url('assets/plugins/jvectormap/jquery-jvectormap-1.2.2.css') ?>">
-        <link rel="stylesheet" href="<? echo base_url('assets/plugins/datepicker/datepicker3.css') ?>">
-        <link rel="stylesheet" href="<? echo base_url('assets/plugins/daterangepicker/daterangepicker-bs3.css') ?>">
-        <link rel="stylesheet" href="<? echo base_url('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') ?>">
+        <link rel="stylesheet" href="../assets/dist/css/adminlte.min.css">
+        <link rel="stylesheet" href="../assets/plugins/iCheck/flat/blue.css">
+        <link rel="stylesheet" href="../assets/plugins/morris/morris.css">
+        <link rel="stylesheet" href="../assets/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+        <link rel="stylesheet" href="../assets/plugins/datepicker/datepicker3.css">
+        <link rel="stylesheet" href="../assets/plugins/daterangepicker/daterangepicker-bs3.css">
+        <link rel="stylesheet" href="../assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
         <style>
             .logonemasakin {
