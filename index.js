c3fb16ba06a08cd2882d922e0e44217ba0082a5c
nugroho <nugrohosamiyono@gmail.com>
2018-07-12 20:00:21 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.sendFile('src/views/login.html'))
-app.get('/:page', (req, res) => res.sendFile('src/views/'+req.page))
+app.get('/', (req, res, next) => {
+
+    var options = {
+      root: __dirname + '/src/views/',
+      dotfiles: 'deny',
+      headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+      }
+    };
+  
+    res.sendFile('login.html', options, function (err) {
+      if (err) {
+        next(err);
+      } else {
+        console.log('Sent:', fileName);
+      }
+    });
+})
+
+app.get('/:page', (req, res, next) => {
+
+    var options = {
+      root: __dirname + '/src/views/',
+      dotfiles: 'deny',
+      headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+      }
+    };
+  
+    var fileName = req.params.page;
+    res.sendFile(fileName+'.html', options, function (err) {
+      if (err) {
+        next(err);
+      } else {
+        console.log('Sent:', fileName);
+      }
+    });
+})
+
+app.use(express.static('src/assets'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
