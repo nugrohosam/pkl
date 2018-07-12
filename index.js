0e78a40d1903ad4c6a5b29674e9e864832dbdc04
Nugroho Samiyono <nugrohosam@192.168.0.10>
2018-07-12 18:36:18 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.sendFile('login.html'))
+app.get('/', (req, res) => res.sendFile('src/views/login.html'))
+app.get('/:page', (req, res) => res.sendFile('src/views/'+req.page))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
