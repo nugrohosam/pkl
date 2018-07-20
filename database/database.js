caec8304586dfa9faf0cfdb7c5c954f152a9f64e
Ubaidillah Hakim <ubaidillahhf@gmail.com>
2018-07-20 23:57:06 +0700

    sekarang

--- database/database.js
+++ database/database.js
@@ -7,5 +7,12 @@ const dbconn = pg.Pool({
     password: 'expressconn',
     port: 5432,
 })
+dbconn.connect(function (err) {
+    if (err) {
+        console.log('Error')
+    } else {
+        console.log('Connected')
+    }
+})
 
 module.exports = dbconn
